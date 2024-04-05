9ad9430f66c9c5f1cfb6356efe3c047dc02d8210
Andn1ght <uzzhanibek.satigul2016@yandex.ru>
2024-04-05 15:59:28 +0500

    12

--- lib/main.dart
+++ lib/main.dart
@@ -23,8 +23,8 @@ class MainScreen extends StatelessWidget {
         children: <Widget>[
           Villain(
             villainAnimation: VillainAnimation.fade(
-              from: Duration(milliseconds: 0),
-              to: Duration(milliseconds: 1000),
+              from: Duration(milliseconds: 500),
+              to: Duration(milliseconds: 1500),
             ),
             child: WaveWidget(
               config: CustomConfig(
@@ -45,8 +45,8 @@ class MainScreen extends StatelessWidget {
           Villain(
             villainAnimation: VillainAnimation.fromBottom(
               relativeOffset: 0.4,
-              from: Duration(milliseconds: 500),
-              to: Duration(milliseconds: 1000),
+              from: Duration(milliseconds: 1000),
+              to: Duration(milliseconds: 2000),
             ),
             child: Center(
               child: Text(
@@ -55,7 +55,41 @@ class MainScreen extends StatelessWidget {
               ),
             ),
           ),
+          Positioned(
+            bottom: 16,
+            left: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Icon(Icons.arrow_forward),
+            ),
+          ),
         ],
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Center(
+        child: Villain(
+          villainAnimation: VillainAnimation.fade(
+            from: Duration(milliseconds: 500),
+            to: Duration(milliseconds: 1500),
+          ),
+          child: Text(
+            'This is the second page!',
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
     );
   }
